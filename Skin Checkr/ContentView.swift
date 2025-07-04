2acb0af806138c8a7ead1d1075870947987c8cf1
William Liu <wliu404@gmail.com>
2025-07-04 14:40:33 +0400

    Initial Commit

---
+++ Skin Checkr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skin Checkr
+//
+//  Created by William Liu on 2025-07-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skin Checkr/Skin_CheckrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skin_CheckrApp.swift
+//  Skin Checkr
+//
+//  Created by William Liu on 2025-07-04.
+//
+
+import SwiftUI
+
+@main
+struct Skin_CheckrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
