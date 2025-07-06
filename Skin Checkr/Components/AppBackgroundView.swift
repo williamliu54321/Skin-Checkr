9fb3bfe766c7dcc57d3d68791ac9a4d27ee91f36
William Liu <wliu404@gmail.com>
2025-07-06 12:37:23 +0400

    [Feature] Added AppBackground View component

---
+++ Skin Checkr/Components/AppBackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  AppBackgroundVIew.swift
+//  Skin Checkr
+//
+//  Created by William Liu on 2025-07-06.
+//
+
+import SwiftUI
+
+struct AppBackgroundVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppBackgroundVIew()
+}
